d63734e138f59419b27de6f8daa8a4eab44dd9b7
Mia <mia.joubert@gmail.com>
2022-04-26 19:40:14 -0500

    search removed from nav

--- react-app/src/components/MainNav.js
+++ react-app/src/components/MainNav.js
@@ -53,12 +53,11 @@ const MainNav = ({ closeSideBar }) => {
               </NavLink>
             </div>
 
-            <div className='nav-bar-item search' >
+            {/* <div className='nav-bar-item search' >
               <div to='/search' exact={true}
                 className="nav-link"
                 id='search-bar'
               >
-                {/* <i className="fa fa-search" aria-hidden="true"></i> */}
                 <input
                   onSubmit={searchSubmit}
                   type='search'
@@ -75,7 +74,7 @@ const MainNav = ({ closeSideBar }) => {
                   <path d="M10.5 3a7.5 7.5 0 015.645 12.438l4.709 4.708a.502.502 0 01-.708.708l-4.708-4.709A7.5 7.5 0 1110.5 3zm0 1a6.5 6.5 0 100 13 6.5 6.5 0 000-13z" fill="currentColor"></path>
                 </svg>
               </div>
-            </div>
+            </div> */}
           </div>
 
           <div className='nav-bar-right'>
